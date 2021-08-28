a6f83df21e3ba2075c062a03eccb7f3d7949214a
Davoyandun <yandundavid@gmail.com>
2021-08-27 21:48:05 -0500

    inicioando las peticiones

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,13 +1,33 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 const { v4: uuidv4 } = require("uuid");
 const axios = require("axios");
- const {Type, Pokemon} = require('../db')
+const { Type, Pokemon } = require("../db");
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+const getPokes = async () => {
+  const req1 = await axios.get("https://pokeapi.co/api/v2/pokemon");
+  const req2 = await axios.get(req1.data.next);
+  const allPokes = req1.data.results.concat(req2.data.results)
+
+  return {
+      name:  await allPokes.map(e=>e.name)
+  }
+
+
+  
+
+
+
+ 
+
+
+}; 
+getPokes().then(e=>console.log(e))
+
 
 module.exports = router;
