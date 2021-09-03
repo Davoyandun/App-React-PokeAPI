78759668a05a628c7b092403261b2700e30677db
Davoyandun <yandundavid@gmail.com>
2021-09-03 12:30:02 -0500

    solucionado problemas de Type

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -5,7 +5,12 @@ export default function Card({ name, img, type, id }) {
     <div key={id}>
       <h2>{name}</h2>
       <img src={img} alt="no found " />
-      <p>Pokemon de tipo: {type}</p>
+      <p>Pokemon de tipo: {type.map(e=>{
+        return <div> {e}</div>
+      }
+      )
+
+      }</p>
     </div>
   );
 }
