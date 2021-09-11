c789e2edc7b38036680287d88af22ccd8ec6ee12
Davoyandun <yandundavid@gmail.com>
2021-09-11 13:53:04 -0500

    creando el componente details

---
+++ client/src/components/Details.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+
+
+export default function Details (){
+ let detail = useSelector(e=> e.detail)
+
+    return(
+        <div>
+            
+
+        </div>
+    )
+}
