bcd9059b0bf51274164a80efa14c9314f371da88
David Yandún <70674906+Davoyandun@users.noreply.github.com>
2021-10-29 02:51:53 -0500

    no

--- client/src/components/Details.jsx
+++ client/src/components/Details.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { Get_Id } from "../actions";
